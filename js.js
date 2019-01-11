ece233b57e3bae93489158ee0049ebc93334139e
Aliaksei <aleksejsch@yandex.by>
2019-01-11 13:20:53 +0300

    js visual

--- js.js
+++ js.js
@@ -8,27 +8,27 @@ toggleUl.on('click', function() {
 
 
 $('.tyings-items').owlCarousel({
-			items: 5,
-			loop: false,
-			dots: false,
-		  	nav: false,
-		  	autoplay: true,
-		  	autoplayTimeout: 2000,
-		  	autoplayHoverPause: true,
-		  	autoplaySpeed: 2000,
-		  	responsive:{
-		  		1025:{
-		  			items:5
-		  		},
-                768:{
-                    items:3
-                },
-		  		0:{
-		  			items:1,
-		  			autoplayTimeout: 5000
-		  		}
-		  	}
-		});
+	items: 5,
+	loop: false,
+	dots: false,
+	nav: false,
+	autoplay: true,
+	autoplayTimeout: 2000,
+	autoplayHoverPause: true,
+	autoplaySpeed: 2000,
+	responsive:{
+		1025:{
+			items:5
+		},
+		768:{
+			items:3
+		},
+		0:{
+			items:1,
+			autoplayTimeout: 5000
+		}
+	}
+});
 
 
 
