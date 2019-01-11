71cb0c04492311e34e190b660d40abd0de064a2d
Romanist <smilemoon@yandex.ru>
2019-01-11 15:55:27 +0300

    smth

--- js.js
+++ js.js
@@ -8,28 +8,49 @@ toggleUl.on('click', function() {
 
 
 $('.tyings-items').owlCarousel({
-			items: 5,
+	items: 5,
+	loop: false,
+	dots: false,
+  	nav: false,
+  	autoplay: true,
+  	autoplayTimeout: 2000,
+  	autoplayHoverPause: true,
+  	autoplaySpeed: 2000,
+  	responsive:{
+  		1025:{
+  			items:5
+  		},
+        768:{
+            items:3
+        },
+  		0:{
+  			items:1,
+  			autoplayTimeout: 5000
+  		}
+  	}
+});
+
+function postsCarousel() {
+	var checkWidth = $(window).width();
+	var owlPost = $('.sub-cats.js-filters form');
+	if (checkWidth > 767) {
+		if(typeof owlPost.data('owl.carousel') != 'undefined'){
+			owlPost.data('owl.carousel').destroy(); 
+		}
+		owlPost.removeClass('owl-carousel');
+	} else if (checkWidth < 768) {
+		owlPost.addClass('owl-carousel');
+		owlPost.owlCarousel({
+			items: 2,
 			loop: false,
 			dots: false,
-		  	nav: false,
-		  	autoplay: true,
-		  	autoplayTimeout: 2000,
-		  	autoplayHoverPause: true,
-		  	autoplaySpeed: 2000,
-		  	responsive:{
-		  		1025:{
-		  			items:5
-		  		},
-                768:{
-                    items:3
-                },
-		  		0:{
-		  			items:1,
-		  			autoplayTimeout: 5000
-		  		}
-		  	}
+		  	nav: false
 		});
+	}
+}
 
+postsCarousel();
+$(window).resize(postsCarousel);	
 
 
 
